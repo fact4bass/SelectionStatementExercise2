5bbca729371f3c18a51c41f49d086653aa2d754e
Juan Santiago <fact4bass@Juans-MBP.fios-router.home>
2021-04-07 15:51:39 -0400

    You got it

---
+++ SelectionStatementsExercise2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool rightAnswer = false;
+
+                var r = new Random();
+                var number = r.Next(1, 1000);
+            while (rightAnswer == false)
+            {
+
+                Console.WriteLine("try to guess my favorite number!");
+
+                var guess = int.Parse(Console.ReadLine());
+
+                if (guess > number)
+
+                { Console.WriteLine("Too high!"); }
+
+                else if (guess < number)
+
+                { Console.WriteLine("Too low!"); }
+
+                else
+                {
+                    Console.WriteLine("You got it! Congratuations!");
+                    rightAnswer = true;
+                }
+
+
+                Console.WriteLine("What is my favorite subject?");
+
+                var subject = Console.ReadLine();
+
+                switch (subject.ToLower())
+                {
+                    case "english":
+                        Console.WriteLine("Would rather do Math.");
+                        break;
+                    case "history":
+                        Console.WriteLine("It's basically English.");
+                        break;
+                    case "math":
+                        Console.WriteLine("How did you know,smartie pants?");
+                        break;
+                    default:
+                        Console.WriteLine("Come on,you should know this!");
+                        break;
+                        
+                }
+
+            }
+
+
+
+        }
+
+    }
+}
